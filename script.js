17671a3d886f52767a9d90484d000b0a1a6bf022
IhorSliepchenko1 <igorslepchenko.cc@gmail.comD>
2023-12-09 18:48:53 +0200

    final

--- script.js
+++ script.js
@@ -37,4 +37,14 @@ function outsideClickHandler(event) {
 	}
 }
 
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+  
 
+  });
