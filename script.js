8ba443a4d6faa829032a96b37a28319857f712b5
IhorSliepchenko1 <igorslepchenko.cc@gmail.comD>
2023-12-09 22:44:05 +0200

    FINAL

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 function navToggle() {
-
-	// Добавляем класс
 	const headerUl = document.getElementById("headerUl");
 	headerUl.classList.toggle("_active");
 
 	const menuIcon = document.getElementById("menu");
 	menuIcon.classList.toggle("_active");
 
-
-
 	const body = document.body;
 	body.classList.toggle("no-scroll");
 	console.log(headerUl, 'Бургер открыт!');
 
 
-	// кликаем куда угодно и бургер закрывается
 	if (menuIcon.classList.contains("_active")) {
 		console.log('Бургер открыт!');
 		document.addEventListener("click", outsideClickHandler);
@@ -22,11 +17,9 @@ function navToggle() {
 		console.log('Бургер закрыт!');
 		document.removeEventListener("click", outsideClickHandler);
 	}
-
 }
 
 
-// проверка что кликнули не по панели бургера, закрываем бургер
 function outsideClickHandler(event) {
 	const headerUl = document.getElementById("headerUl");
 	const menuIcon = document.getElementById("menu");
